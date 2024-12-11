f63c6c9398a57168b96d3137cf8e04ddc12f55de
samarth <samganamote@gmail.com>
2024-12-11 22:08:51 +0530

    chnages made at password

--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -7,31 +7,17 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function SignUp() {
 const [input ,setinput]=useState({name:"",email:"",password:""})
-const [pass,setpass]=useState("");
-const [repass,setrepass]=useState("");
+
 const change=(e)=>{
 const {name,value}=e.target;
 setinput({...input,[name]:value})
 }
-const createpass=()=>{
-  console.log(pass,repass);
-  if(pass===repass){
-    setinput(input=>({...input,password :repass}))
-    console.log(input);
-    console.log("password inserted",pass);
-  }
-  else{
-    toast.error("password must be same ",{autoClose:1500})
-    setpass("");
-    setrepass("")
-  }
-}
+
 const submit=(e)=>{
     e.preventDefault();
 try {
-  createpass();
-  console.log(input);  
-  setinput({name:"",email:"",})
+  console.log(input);
+  setinput({name:"",email:"",  password:""})
 } catch (error) {
 
   console.log(error);
@@ -51,10 +37,9 @@ try {
         <input type="text"  onChange={change} value={input.name} name="name"/>
         <label htmlFor="" >Email</label>
         <input type="text"  onChange={change} value={input.email} name="email"/>
-        <label htmlFor="" >Create Password</label>
-        <input type="password"  onChange={(e)=>setpass(e.target.value)} name="pass" value={pass}/>
-        <label htmlFor="" > Password</label>
-        <input type="password"  onChange={(e)=>setrepass(e.target.value)}  name="re-password" value={repass}/>
+        <label htmlFor="" >Create and insert  Password</label>
+        <input type="password"  onChange={change} name="password" value={input.password}/>
+       
         <button onClick={submit}  type ='submit'> SignUp</button>
         <p> ALready have an account ? 
           <Link to='/'>LOG-IN</Link>
